de45d005bbf4e6e170e456ff93d805d9ae48e371
jingjing-maker <80103529+jingjing-maker@users.noreply.github.com>
2023-04-05 23:07:04 +0800

    Update __init__.py

--- SSmokeNet/models/__init__.py
+++ SSmokeNet/models/__init__.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 # Copyright (c) 2014-2021 Megvii Inc. All rights reserved.
 
-from .darknet import Darknet_S
+from .darknet import SNet
 from .losses import IOUloss
 from .backbone import PAFPN
 from .head import Head
